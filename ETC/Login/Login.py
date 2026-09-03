c1a3946b3178b9e3fb500e9444a6844ad24cd226
Joaquin Gil <joaquingil9308@gmail.com>
2026-09-03 11:06:23 -0300

    Act

--- ETC/Login/Login.py
+++ ETC/Login/Login.py
@@ -1,16 +1,28 @@
 print("Bienvenido a Scape The Code")
 
+def encriptado(contra):
+    resultado = ""
+    for caracter in contra:
+        codigo= ord(caracter)
+        n_cod = codigo + 4
+        resultado += chr(n_cod)
+    return resultado
+        
 def login(usuario,contra):
     log = False
     while log == False:
         log_us = input("Ingrese su usuario: ")
         log_pasword = input("Ingrese su contraseña: ")
+        log_pasword = encriptado(log_pasword)
         if log_us == usuario and log_pasword == contra:
             print("Inisiaste sesion como:",usuario)
             log = True
         else:
             print("Error - Usuario o Contraseña incorrectas")
-
     return log
 
-log = login("pepe","wzq")
+us1 = "pepe"
+contra1 = encriptado("pepito123")
+print(contra1)
+
+log = login(us1, contra1)
